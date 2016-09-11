79ddbb1eb42c38b6aeef2fefb330d41a9d00c658
Paolo Scanferla <paolo.scanferla@gmail.com>
2016-09-11 14:51:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# eslint-config-staticdeploy
+
+Staticdeploy eslint configuration.
+
+## Install
+
+```sh
+npm i --save-dev eslint-config-staticdeploy
+```
+
+## Usage
+
+Add to your `.eslintrc`:
+
+  * plain javascript projects
+    ```json
+    {
+        "extends": ["eslint-config-staticdeploy"]
+    }
+    ```
+
+  * react projects projects:
+    ```json
+    {
+        "extends": ["eslint-config-staticdeploy/react"]
+    }
+    ```
---
+++ react.js
@@ -0,0 +1,63 @@
+module.exports = {
+    "extends": ["./index.js"],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    }
+    "plugins": ["react"],
+    "rules": {
+        // Base rules
+        "react/display-name": [0],
+        "react/forbid-component-props": [0],
+        "react/forbid-prop-types": [0],
+        "react/no-danger": [0],
+        "react/no-danger-with-children": [0],
+        "react/no-deprecated": [2],
+        "react/no-did-mount-set-state": [2],
+        "react/no-did-update-set-state": [2],
+        "react/no-direct-mutation-state": [2],
+        "react/no-find-dom-node": [2],
+        "react/no-is-mounted": [2],
+        "react/no-multi-comp": [0],
+        "react/no-render-return-value": [2],
+        "react/no-set-state": [0],
+        "react/no-string-refs": [2],
+        "react/no-unknown-property": [2],
+        "react/no-unused-prop-types": [2],
+        "react/prefer-es6-class": [2],
+        "react/prefer-stateless-function": [0],
+        "react/prop-types": [2],
+        "react/react-in-jsx-scope": [2],
+        "react/require-optimization": [0],
+        "react/require-render-return": [2],
+        "react/self-closing-comp": [2],
+        "react/sort-comp": [2],
+        "react/sort-prop-types": [2],
+        "react/style-prop-object": [2],
+        // JSX specific rules
+        "react/jsx-boolean-value": [2, "always"],
+        "react/jsx-closing-bracket-location": [2],
+        "react/jsx-curly-spacing": [2],
+        "react/jsx-equals-spacing": [2],
+        "react/jsx-filename-extension": [2],
+        "react/jsx-first-prop-new-line": [2, "multiline"],
+        "react/jsx-handler-names": [0],
+        "react/jsx-indent": [2],
+        "react/jsx-indent-props": [2],
+        "react/jsx-key": [2],
+        "react/jsx-max-props-per-line": [0],
+        "react/jsx-no-bind": [0],
+        "react/jsx-no-comment-textnodes": [2],
+        "react/jsx-no-duplicate-props": [2],
+        "react/jsx-no-literals": [2],
+        "react/jsx-no-target-blank": [2],
+        "react/jsx-no-undef": [2],
+        "react/jsx-pascal-case": [2],
+        "react/jsx-sort-props": [2],
+        "react/jsx-space-before-closing": [2, "always"],
+        "react/jsx-uses-react": [2],
+        "react/jsx-uses-vars": [2],
+        "react/jsx-wrap-multilines": [2]
+    }
+};
